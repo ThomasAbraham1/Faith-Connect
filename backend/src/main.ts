57871057e6f97b6e1c89f860eeb7c5970d8d46d6
Thomas Abraham <cta102938@gmail.com>
2025-10-09 20:34:18 +0530

    going to publish 3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const isProduction = process.env.NODE_ENV == 'production';
   app.enableCors({
-    origin: isProduction ? 'http://localhost:5173' : 'http://localhost:5173',
+    origin: isProduction ? 'https://faith-connect.onrender.com/' : 'http://localhost:5173',
     credentials: true,
   });
   // Global Filter
