ba7babe377cfb17befe51eda902d80a9620dbffd
Thomas Abraham <cta102938@gmail.com>
2025-10-10 01:03:25 +0530

    going to publish 8

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
         element: <LoginForm />,
       },
       {
-        path: "signupp",
+        path: "Signup",
         element: <Hello />,
       },
       {
