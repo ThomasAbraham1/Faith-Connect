b08bcde3d705c65ababf31300545749ea6bebc13
Thomas Abraham <cta102938@gmail.com>
2025-10-10 16:57:10 +0530

    going to publish 25

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,22 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const isProduction = process.env.NODE_ENV == 'production';
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'https://effervescent-beignet-60b0fd.netlify.app',
-        'http://localhost:5173',
-        'https://faith-connect.onrender.com',
-        'https://faithconnect-474707.el.r.appspot.com',
-        'https://www.faithconnect.store',
-      ];
-      console.log('Request Origin:', origin); // Log the origin
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    origin: 'https://www.faithconnect.store',
     credentials: true,
   });
   console.log(isProduction)
@@ -48,19 +33,15 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 60 * 60 * 1000 * 24 * 365,
-        secure: isProduction ? true : false,
-        sameSite:'lax',
+        secure: true,
+        sameSite: 'lax',
       },
     }),
   );
-
   // console.log(process.env.JWT_SECRET)
   app.use(passport.initialize());
   app.use(passport.session());
-  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
-  const host = '0.0.0.0'; // Required for Cloud Run
-  await app.listen(port, host);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  // await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
