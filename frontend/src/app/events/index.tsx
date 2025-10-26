97945db46f2e08e4cba7dffe49a01894ef52893a
titusbonke <titusbonkec@gmail.com>
2025-10-26 22:34:03 +0530

    few more changes on dynamic

--- frontend/src/app/events/index.tsx
+++ frontend/src/app/events/index.tsx
@@ -1,10 +1,18 @@
 import { useUser } from "@/context/UserProvider";
 import { useQuery } from "@tanstack/react-query";
 import api from "@/api/api";
+import { DynamicTable1 } from "@/components/dynamic/DynamicTable1";
+import type { Table } from "@tanstack/react-table";
+import { useRef } from "react";
+import { ActionsColumn } from "@/components/dynamic/ActionsColumn";
+import { Eye, SquarePen, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 
 function EventsPage() {
   const userContext = useUser()
+  const tableRef = useRef<Table<unknown>>(null);
+
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: ["eventsData"],
     queryFn: async () => {
@@ -17,7 +25,31 @@ function EventsPage() {
   if (error) return <div>Error occurred: {(error as Error).message}</div>
   if (!isFetching) {
     console.log(data?.data?.data, 'events data');
-    return (<h1>Hello World! 1</h1>);
+    let dataArray = data?.data?.data.map((a: any) => ({
+      eventName: a.eventName,
+      eventDate: a.eventDate,
+      description: a.description,
+      eventLocation: a.eventLocation,
+      organizer: a.organizer,
+      createdDate: a.createdAt,
+    }));
+    return (
+      <DynamicTable1 ref={tableRef} data={dataArray}
+      // columnOptions={{ HideColumns: ["_id"] }}
+      >
+        <ActionsColumn >
+          <Button variant={"ghost"}>
+          <SquarePen></SquarePen>
+          </Button>
+          <Button variant={"ghost"} >
+          <Trash2></Trash2>
+          </Button>
+          {/* <Button variant={"ghost"}>
+          <Eye></Eye>
+          </Button> */}
+        </ActionsColumn>
+      </DynamicTable1>
+    );
   }
 }
 export default EventsPage;
